001947434b64e516286edec60ebcec01f61a3516
klyuchevsky <juhn@bk.ru>
2013-04-19 17:48:57 +0700

    Add hash map to module. Not working copy.

--- Less2/src/com/suhorukov/klyuchevsky/Calculator.java
+++ Less2/src/com/suhorukov/klyuchevsky/Calculator.java
@@ -1,18 +1,32 @@
 package com.suhorukov.klyuchevsky;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Calculator {
     public static void main(String[] args) {
         Stack<Double> stack = new Stack<>();
+        Map<String, Object> map = new HashMap<String, Object>();
         String string = "";
 
+        map.put("PUSH", new Push());
+        map.put("POP", new Pop());
+        System.out.println(map.keySet());
+        System.out.println(map.values());
 
         while (true) {
             Scanner sc = new Scanner(System.in);
             string = sc.nextLine();
             String[] words = string.split(" ");
+
+//            if ("PUSH".equals(words[1])){
+//                System.out.println("test");
+//            }
+
+            for (String w : words) {
+                System.out.println(w);
+            }
+
+
 //            new Push().execute();
 
 
---
+++ Less2/src/com/suhorukov/klyuchevsky/Pop.java
@@ -0,0 +1,11 @@
+package com.suhorukov.klyuchevsky;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class Pop implements Command {
+    public void execute(Stack<Double> stack, String string, Map<String, Double> map) {
+
+    }
+
+}
