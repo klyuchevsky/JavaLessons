2e4d86f113169adcaf2a8cba5eca4604e136103c
Klyuchevsky Eugene <juhn@bk.ru>
2013-04-22 14:04:02 +0700

    Modify Less2: implemented all operations.

--- Less2/src/com/suhorukov/klyuchevsky/Push.java
+++ Less2/src/com/suhorukov/klyuchevsky/Push.java
@@ -7,8 +7,19 @@ public class Push implements Command {
     public void execute(Stack<Double> stack, String string, Map<String, Double> variables) {
         int i;
         String[] words = string.split(" ");
+
+        if (words.length < 2) {
+            System.out.println("Недостаточно параметров, введите значение для помещения в стек");
+            return;
+        }
+
         for (i = 1; i < words.length; i++)
             try {
+                if (!Character.isDigit(words[i].charAt(0))) {
+                    if (variables.containsKey(words[i])) {
+                        words[i] = variables.get(words[i]).toString();
+                    }
+                }
                 stack.push(Double.parseDouble(words[i]));
             } catch (NumberFormatException e) {
                 System.out.println("Не могу положить в стек значение: " + words[i]);
