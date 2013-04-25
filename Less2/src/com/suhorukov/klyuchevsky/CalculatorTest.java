1c7f3622eaeddc80085dc09e9c1e04121f8243d0
klyuchevsky <juhn@bk.ru>
2013-04-25 17:18:31 +0700

    Modify Less2: trying create unit test.

--- Less2/src/com/suhorukov/klyuchevsky/CalculatorTest.java
+++ Less2/src/com/suhorukov/klyuchevsky/CalculatorTest.java
@@ -2,11 +2,15 @@ package com.suhorukov.klyuchevsky;
 
 import org.junit.Test;
 
+
 public class CalculatorTest {
 
 
     @Test
     public void testMain() throws Exception {
-
+        Calculator calc = new Calculator();
+//        Calculator.string = "push 3";
     }
+
+
 }
